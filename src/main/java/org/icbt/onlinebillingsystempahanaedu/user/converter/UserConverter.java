72b21d6b6b5c0bc033720d8a7d48c127a9550fd4
username <usermail>
2025-07-25 20:39:05 +0530

    Refactor: update UserConverter

--- src/main/java/org/icbt/onlinebillingsystempahanaedu/user/converter/UserConverter.java
+++ src/main/java/org/icbt/onlinebillingsystempahanaedu/user/converter/UserConverter.java
@@ -3,6 +3,9 @@ package org.icbt.onlinebillingsystempahanaedu.user.converter;
 import org.icbt.onlinebillingsystempahanaedu.user.dto.UserDTO;
 import org.icbt.onlinebillingsystempahanaedu.user.entity.UserEntity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * author : Niwanthi
  * date : 7/22/2025
@@ -19,7 +22,40 @@ public class UserConverter {
         userEntity.setUsername(userDTO.getUsername());
         userEntity.setPassword(userDTO.getPassword());
         userEntity.setRole(userDTO.getRole());
+        userEntity.setCreatedAt(userDTO.getCreatedAt());
+        userEntity.setUpdatedAt(userDTO.getUpdatedAt());
+        userEntity.setDeletedAt(userDTO.getDeletedAt());
         
         return userEntity;
     }
+
+    //Convert Entity to DTO
+    public static UserDTO convertUserEntityToUserDTO(UserEntity userEntity) {
+        if (userEntity == null){
+            return null;
+        }
+
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(userEntity.getId());
+        userDTO.setUsername(userEntity.getUsername());
+        userDTO.setPassword(userEntity.getPassword());
+        userDTO.setRole(userEntity.getRole());
+        userDTO.setCreatedAt(userEntity.getCreatedAt());
+        userDTO.setUpdatedAt(userEntity.getUpdatedAt());
+        userDTO.setDeletedAt(userEntity.getDeletedAt());
+
+        return userDTO;
+    }
+
+    public static List<UserDTO> convertUserEntityToUserDTOList(List<UserEntity> userEntityList) {
+        List<UserDTO> userDTOList = new ArrayList<>();
+        if (userEntityList != null){
+            for (UserEntity userEntity : userEntityList) {
+                userDTOList.add(convertUserEntityToUserDTO(userEntity));
+            }
+        }
+        return userDTOList;
+    }
+
+
 }
