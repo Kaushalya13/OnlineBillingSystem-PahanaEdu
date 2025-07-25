edca16e51b7953233384d0e9c00939793c032c85
username <usermail>
2025-07-25 13:55:06 +0530

    feat: add UserConverter class to convert UserDTO to UserEntity

--- src/main/java/org/icbt/onlinebillingsystempahanaedu/user/converter/UserConverter.java
+++ src/main/java/org/icbt/onlinebillingsystempahanaedu/user/converter/UserConverter.java
@@ -1,9 +1,25 @@
 package org.icbt.onlinebillingsystempahanaedu.user.converter;
 
+import org.icbt.onlinebillingsystempahanaedu.user.dto.UserDTO;
+import org.icbt.onlinebillingsystempahanaedu.user.entity.UserEntity;
+
 /**
  * author : Niwanthi
  * date : 7/22/2025
  * time : 7:09 PM
  */
 public class UserConverter {
+    // Convert DTO to Entity
+    public static UserEntity convertUserToUserEntity(UserDTO userDTO) {
+        if (userDTO == null){
+            return null;
+        }
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(userDTO.getId());
+        userEntity.setUsername(userDTO.getUsername());
+        userEntity.setPassword(userDTO.getPassword());
+        userEntity.setRole(userDTO.getRole());
+        
+        return userEntity;
+    }
 }
