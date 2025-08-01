bb8bdad71f67295cfb34b07be4c0c23a03cfc356
Kaushalya13 <nimanthikaushalya13@gmail.com>
2025-08-01 21:13:25 +0530

    Refactor: implement DAOUtil with executeUpdate, executeQuery methods

--- src/main/java/org/icbt/onlinebillingsystempahanaedu/core/util/DAOUtil.java
+++ src/main/java/org/icbt/onlinebillingsystempahanaedu/core/util/DAOUtil.java
@@ -2,7 +2,10 @@ package org.icbt.onlinebillingsystempahanaedu.core.util;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * author : Niwanthi
@@ -10,7 +13,52 @@ import java.sql.SQLException;
  * time : 7:59 PM
  */
 public class DAOUtil {
-    public static <T> T executeQueryOrUpdate(Connection connection, String sql, Object... args) throws SQLException {
+    private static final Logger logger = Logger.getLogger(DAOUtil.class.getName());
+
+    private DAOUtil() {
+
+    }
+
+    //Execute an sql UPDATE , INSERT or DELETE statement.
+    public static boolean executeUpdate(Connection connection, String sql, Object... args) {
+        try (PreparedStatement preparedStatement  = connection.prepareStatement(sql)){
+            for (int i = 0; i < args.length; i++) {
+                preparedStatement.setObject(i + 1, args[i]);
+            }
+                return preparedStatement .executeUpdate() > 0;
+        } catch (SQLException e) {
+            logger.log(Level.SEVERE, "SQL Update/Insert/Delete Error: " + sql + " - " + e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    //Execute an SQL SELECT statement
+    public static ResultSet executeQuery(Connection connection, String sql, Object... args) {
+        PreparedStatement preparedStatement = null;
+
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+                preparedStatement.setObject((i + 1),args[i]);
+            }
+            return preparedStatement.executeQuery();
+        }catch (SQLException e) {
+            logger.log(Level.SEVERE, "SQL SELECT Error: " + sql + " - " + e.getMessage(), e);
+            //close the statement if an error occurs before returning ResultSet
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                }catch (SQLException e1) {
+                    logger.log(Level.WARNING, "SQL CLOSE Error: " + sql + " - " + e.getMessage(), e);
+                }
+            }
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public static <T> T executeSql(Connection connection, String sql, Object... args) throws SQLException {
 
         try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
 
