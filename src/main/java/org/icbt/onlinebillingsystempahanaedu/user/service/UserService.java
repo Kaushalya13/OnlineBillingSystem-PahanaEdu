099f8f04d223409f68b3c6be29b096b9024b98f9
Kaushalya13 <nimanthikaushalya13@gmail.com>
2025-08-06 22:11:33 +0530

    Refactor(user): add loginUser method to UserService interface for authentication support

--- src/main/java/org/icbt/onlinebillingsystempahanaedu/user/service/UserService.java
+++ src/main/java/org/icbt/onlinebillingsystempahanaedu/user/service/UserService.java
@@ -3,10 +3,15 @@ package org.icbt.onlinebillingsystempahanaedu.user.service;
 import org.icbt.onlinebillingsystempahanaedu.core.repo.CrudService;
 import org.icbt.onlinebillingsystempahanaedu.user.dto.UserDTO;
 
+import java.sql.SQLException;
+
 /**
  * author : Niwanthi
  * date : 7/22/2025
  * time : 7:16 PM
  */
 public interface UserService extends CrudService<UserDTO> {
+
+    UserDTO loginUser(String username, String password) throws SQLException, ClassNotFoundException;
+
 }
